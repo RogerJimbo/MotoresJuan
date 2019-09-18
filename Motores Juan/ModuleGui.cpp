29887fe8f3b1f404d8360393f8f82fabbbc7ccb0
RogerJimbo <roger.sanchezmarch@hotmail.es>
2019-09-18 16:14:53 +0200

    Added project

---
+++ Motores Juan/ModuleGui.cpp
@@ -0,0 +1,56 @@
+#include "Globals.h"
+#include "Application.h"
+#include "ModuleGUI.h"
+
+#include "ImGui\imgui.h"
+#include "ImGui\imgui_impl_opengl3.h"
+
+#include "ModuleWindow.h"
+#include "ModuleInput.h"
+#include "ModuleTextures.h"
+
+
+ModuleGui::ModuleGui(Application* app, bool start_enabled) : Module(app, start_enabled)
+{
+}
+
+ModuleGui::~ModuleGui()
+{
+}
+
+
+bool ModuleGui::Init()
+{
+	IMGUI_CHECKVERSION();
+	ImGui::CreateContext();
+	ImGuiIO& io = ImGui::GetIO();
+
+
+	return true;
+}
+
+// PreUpdate: clear buffer
+update_status ModuleGui::PreUpdate(float dt)
+{
+
+	return UPDATE_CONTINUE;
+}
+
+// Update: debug camera
+update_status ModuleGui::Update()
+{
+	return UPDATE_CONTINUE;
+}
+
+// PostUpdate present buffer to screen
+update_status ModuleGui::PostUpdate()
+{
+	return UPDATE_CONTINUE;
+}
+
+// Called before quitting
+bool ModuleGui::CleanUp()
+{
+
+	return true;
+}
---
+++ Motores Juan/ModuleGui.h
@@ -0,0 +1,22 @@
+#ifndef __MODULEGUI_H__
+#define __MODULEGUI_H__
+
+#include "Module.h"
+
+class ModuleGui : public Module
+{
+public:
+	ModuleGui(Application* app, bool start_enabled = true);
+	~ModuleGui();
+
+	bool Init();
+	update_status PreUpdate(float dt);
+	update_status Update();
+	update_status PostUpdate();
+	bool CleanUp();
+
+
+
+};
+
+#endif // !__MODULEGUI_H__
