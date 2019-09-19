1523dacc1bccd60412df27bccb779cd622c38745
RogerJimbo <roger.sanchezmarch@hotmail.es>
2019-09-19 19:49:43 +0200

    Added MathGeoLib and Bullet

--- Motores Juan/ModuleGui.cpp
+++ Motores Juan/ModuleGui.cpp
@@ -6,6 +6,13 @@
 #include "ImGui\imgui_impl_sdl.h"
 #include "ImGui\imgui_impl_opengl2.h"
 
+#include "MathGeoLib/Geometry/Triangle.h"
+#include "MathGeoLib/Geometry/TriangleMesh.h"
+#include "MathGeoLib/MathGeoLib.h"
+
+#include "MathGeoLib/Math/myassert.h"
+
+
 #include "ModuleWindow.h"
 
 ModuleGui::ModuleGui(Application* app, bool start_enabled) : Module(app, start_enabled)
@@ -37,6 +44,9 @@ update_status ModuleGui::PreUpdate(float dt)
 	ImGui_ImplSDL2_NewFrame(App->window->window);
 	ImGui::NewFrame();
 
+	ImVec2 win_1_pos = ImVec2(50.0f, 50.0f);	
+	ImGui::SetWindowPos(win_1_pos,0);
+
 	return UPDATE_CONTINUE;
 }
 
@@ -45,9 +55,7 @@ update_status ModuleGui::Update(float dt)
 {
 	if (show_demo_window)
 		ImGui::ShowDemoWindow(&show_demo_window);
-	ImGui::End();
 
-	ImGui::Text("Hola");
 
 	return UPDATE_CONTINUE;
 }
@@ -65,7 +73,8 @@ update_status ModuleGui::PostUpdate(float dt)
 // Called before quitting
 bool ModuleGui::CleanUp()
 {
-
+	ImGui::End();
+	ImGui::EndChild();
 
 	ImGui_ImplOpenGL2_Shutdown();
 	ImGui_ImplSDL2_Shutdown();
