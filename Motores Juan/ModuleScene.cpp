48681c4ee988a4a4e083f5bfe84fef263c7ffe0c
FurryGhoul <ivandrofiak@hotmail.com>
2019-10-05 14:44:57 +0200

    Scene module works

--- Motores Juan/ModuleScene.cpp
+++ Motores Juan/ModuleScene.cpp
@@ -13,27 +13,27 @@ ModuleScene::~ModuleScene() {}
 
 bool ModuleScene::Init(const JSON_Object& config)
 {
-
+	return true;
 }
 
 bool ModuleScene::Start()
 {
-
+	return true;
 }
 
 update_status ModuleScene::PreUpdate(float dt)
 {
-
+	return UPDATE_CONTINUE;
 }
 
 update_status ModuleScene::Update(float dt)
 {
-
+	return UPDATE_CONTINUE;
 }
 
 update_status ModuleScene::PostUpdate(float dt)
 {
-
+	return UPDATE_CONTINUE;
 }
 
 void ModuleScene::Draw()
@@ -43,7 +43,7 @@ void ModuleScene::Draw()
 
 bool ModuleScene::CleanUp()
 {
-
+	return true;
 }
 
 void ModuleScene::Save_Config(JSON_Object& config) const
