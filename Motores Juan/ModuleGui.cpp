757bd0b21dbb073b390897c0a4e69ae3f0999f37
Roger Sanchez March <roger.sanchezmarch@hotmail.es>
2019-10-11 10:02:34 +0200

    Finished Docking

--- Motores Juan/ModuleGui.cpp
+++ Motores Juan/ModuleGui.cpp
@@ -70,7 +70,6 @@ update_status ModuleGui::PreUpdate(float dt)
 update_status ModuleGui::Update(float dt)
 {
 	Docking();
-
 	//ImGui::ShowDemoWindow();
 
 	//Engine Windows
@@ -226,21 +225,39 @@ void ModuleGui::CreateHardwareWindow(bool* open)
 
 void ModuleGui::Docking()
 {
-	ImGuiWindowFlags window_flags = ImGuiDockNodeFlags_None;
-	//ImGuiViewport* viewport = ImGui::GetWindowViewport();
+	static bool open = true;
+	bool opt_fullscreen = open;
+	static ImGuiDockNodeFlags dockspace_flags = ImGuiDockNodeFlags_None;
+	ImGuiWindowFlags window_flags = ImGuiWindowFlags_MenuBar | ImGuiWindowFlags_NoDocking;
 
-	ImGui::PushStyleVar(ImGuiStyleVar_WindowRounding, 0.0f);
-	ImGui::PushStyleVar(ImGuiStyleVar_WindowBorderSize, 0.0f);
+	if (opt_fullscreen)
+	{
+		ImGuiViewport* viewport = ImGui::GetMainViewport();
+		ImGui::SetNextWindowPos(viewport->Pos);
+		ImGui::SetNextWindowSize(viewport->Size);
+		ImGui::SetNextWindowViewport(viewport->ID);
+		ImGui::PushStyleVar(ImGuiStyleVar_WindowRounding, 0.0f);
+		ImGui::PushStyleVar(ImGuiStyleVar_WindowBorderSize, 0.0f);
+		window_flags |= ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove;
+		window_flags |= ImGuiWindowFlags_NoBringToFrontOnFocus | ImGuiWindowFlags_NoNavFocus;
+	}
+
+	if (dockspace_flags & ImGuiDockNodeFlags_PassthruCentralNode)	window_flags |= ImGuiWindowFlags_NoBackground;
+
 	ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(0.0f, 0.0f));
+	ImGui::Begin("DockSpace", &open, window_flags);
+	ImGui::PopStyleVar();
 
-	ImGui::PopStyleVar(); 	ImGui::PopStyleVar(2);
+	if (opt_fullscreen)	ImGui::PopStyleVar(2);
 
 	ImGuiIO& io = ImGui::GetIO();
 	if (io.ConfigFlags & ImGuiConfigFlags_DockingEnable)
 	{
-		ImGuiID dockspace_id = ImGui::GetID("Docking");
-		ImGui::DockSpace(dockspace_id, ImVec2(0.0f, 0.0f), window_flags);
+		ImGuiID dockspace_id = ImGui::GetID("MyDockSpace");
+		ImGui::DockSpace(dockspace_id, ImVec2(0.0f, 0.0f), dockspace_flags);
 	}
+
+	ImGui::End();
 }
 
 void ModuleGui::Save_Config(JSON_Object& config) const
