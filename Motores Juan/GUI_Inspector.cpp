11e6f49a837591a6916d189c095dc950871d7159
RogerJimbo <roger.sanchezmarch@hotmail.es>
2019-10-23 13:39:04 +0200

    Improved Inspector

--- Motores Juan/GUI_Inspector.cpp
+++ Motores Juan/GUI_Inspector.cpp
@@ -12,18 +12,26 @@ void GUI_Inspector::Draw(bool* open)
 	if (ImGui::Begin(elem_name.c_str(), open))
 	{
 		bool foo = false;
-		ImGui::Checkbox("", &foo);
+		static char bufpass[64] = "Game Object Name";
+		ImGui::Checkbox("", &foo); ImGui::SameLine();
+		ImGui::InputText(" ", bufpass, 64, ImGuiInputTextFlags_CharsNoBlank);
 	
-		if (ImGui::CollapsingHeader("Transform"))
+		if (ImGui::CollapsingHeader("Transform"), ImGuiTreeNodeFlags_OpenOnArrow)
 		{
-			static int vecpos[3] = { 0, 0, 0 }, vecrot[3] = { 0, 0, 0 }, vecscale[3] = { 0, 0, 0 };
+			static int vecpos[3] = { 0, 0, 0 }, vecrot[3] = { 0, 0, 0 }, vecscale[3] = { 1, 1, 1 };
 
 			ImGui::Text("X         Y         Z");
 			ImGui::DragInt3("Position", vecpos, 0.25f);
 			ImGui::Text("X         Y         Z");
-			ImGui::DragInt3("Rotation", vecrot, 0.25f);
+			ImGui::SliderInt3("Rotation", vecrot, 0.0f, 360.0f);
 			ImGui::Text("X         Y         Z");
-			ImGui::DragInt3("Scale", vecscale, 0.25f);
+			ImGui::DragInt3("Scale", vecscale, 0.25f, 0.0f, 1000.0f);
+
+
+
+
+			
+
 		}
 	}
 	ImGui::End();
