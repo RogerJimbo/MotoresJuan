a48c8e60082ff21301d294e556f1870b9b362500
RogerJimbo <roger.sanchezmarch@hotmail.es>
2019-10-23 13:06:53 +0200

    started Inspector

--- Motores Juan/GUI_Inspector.cpp
+++ Motores Juan/GUI_Inspector.cpp
@@ -11,9 +11,20 @@ void GUI_Inspector::Draw(bool* open)
 {
 	if (ImGui::Begin(elem_name.c_str(), open))
 	{
+		bool foo = false;
+		ImGui::Checkbox("", &foo);
+	
+		if (ImGui::CollapsingHeader("Transform"))
+		{
+			static int vecpos[3] = { 0, 0, 0 }, vecrot[3] = { 0, 0, 0 }, vecscale[3] = { 0, 0, 0 };
 
-		
-
+			ImGui::Text("X         Y         Z");
+			ImGui::DragInt3("Position", vecpos, 0.25f);
+			ImGui::Text("X         Y         Z");
+			ImGui::DragInt3("Rotation", vecrot, 0.25f);
+			ImGui::Text("X         Y         Z");
+			ImGui::DragInt3("Scale", vecscale, 0.25f);
+		}
 	}
 	ImGui::End();
 }
