400d8dae8e8060de3ff212592218b0bcd0069dd2
FurryGhoul <ivandrofiak@hotmail.com>
2019-10-27 14:49:02 +0100

    Started gameobject inspector

--- Motores Juan/GUI_Inspector.cpp
+++ Motores Juan/GUI_Inspector.cpp
@@ -2,6 +2,8 @@
 #include "Globals.h"
 #include "Application.h"
 #include "ModuleGui.h"
+#include "GameObject.h"
+#include "ComponentMesh.h"
 
 GUI_Inspector::GUI_Inspector(Application* app, bool start_enabled) : GUI_Element(app, start_enabled) { elem_name = "Inspector"; }
 
@@ -18,23 +20,48 @@ void GUI_Inspector::Draw(bool* open)
 		ImGui::Checkbox("Active", &foo); ImGui::SameLine();
 		ImGui::InputText(" ", bufpass, 64, ImGuiInputTextFlags_CharsNoBlank);
 
-		
-		if ((ImGui::CollapsingHeader("Transform")) )
+		for (auto item = App->modscene->gameobjects.begin(); item != App->modscene->gameobjects.end(); ++item)
 		{
-			ImGui::Checkbox("Active", &transform);
-			static int vecpos[3] = { 0, 0, 0 }, vecrot[3] = { 0, 0, 0 }, vecscale[3] = { 1, 1, 1 };
+			if ((*item)->selected && (*item)->children.size() > 0)
+			{
+				selectedGO = (*item);
 
-			ImGui::DragInt3("Position", vecpos, 0.25f);
-			ImGui::SliderInt3("Rotation", vecrot, 0.0f, 360.0f);
-			ImGui::DragInt3("Scale", vecscale, 0.25f, 1.0f, 1000.0f);
+				if ((ImGui::CollapsingHeader("Transform")))
+				{
+					ImGui::Checkbox("Active", &transform);
+					static int vecpos[3] = { 0, 0, 0 }, vecrot[3] = { 0, 0, 0 }, vecscale[3] = { 1, 1, 1 };
 
-			ImGuiIO& io = ImGui::GetIO();
+					ImGui::DragInt3("Position", vecpos, 0.25f);
+					ImGui::SliderInt3("Rotation", vecrot, 0.0f, 360.0f);
+					ImGui::DragInt3("Scale", vecscale, 0.25f, 1.0f, 1000.0f);
 
-			io.WantCaptureKeyboard;
+					ImGuiIO& io = ImGui::GetIO();
 
-			//ImGui::ShowStyleEditor();
+					io.WantCaptureKeyboard;
 
-		} 
+					//ImGui::ShowStyleEditor();
+
+				}
+
+				if ((ImGui::CollapsingHeader("Mesh")))
+				{
+					meshes = new ComponentMesh();
+					for (auto item = selectedGO->children.begin(); item != selectedGO->children.end(); ++item)
+					{
+						ComponentMesh*auxmesh = (ComponentMesh*)(*item)->GetComponent(MESH);
+						meshes->num_vertices += auxmesh->num_vertices;
+					}
+
+					ImGui::Text("Number of vertices: %d", meshes->num_vertices);
+					ImGui::Text("Number of children: %d", selectedGO->children.size());
+				}
+
+				if ((ImGui::CollapsingHeader("Material")))
+				{
+
+				}
+			}
+		}
 	}
 	ImGui::End();
 }
